52cbe8dc7496010f337553d72501cc99124c2ad7
Arne-Christian Rundereim <arne.christian.rundereim@bartec-pixavi.com>
2023-04-10 17:41:25 +0200

    c2bd: Add possibility to exclude additional dates

--- c2bd/src/lib.rs
+++ c2bd/src/lib.rs
@@ -1,20 +1,39 @@
 use chrono::{DateTime, Datelike, Days, FixedOffset, Weekday};
-use std::ops::Add;
 
-pub fn calculate_business_days_between<Tz>(start_date: DateTime<Tz>, end_date: DateTime<Tz>) -> i32
-where
-    Tz: chrono::TimeZone,
-{
-    let mut business_days = 0;
-    let mut current_date = start_date;
-    while current_date <= end_date {
-        if current_date.weekday() != Weekday::Sat && current_date.weekday() != Weekday::Sun {
-            business_days += 1;
+struct BusinessDays {
+    start_date: DateTime<FixedOffset>,
+    end_date: DateTime<FixedOffset>,
+    excluded_days: Vec<DateTime<FixedOffset>>,
+}
+
+impl BusinessDays {
+    pub fn new(start_date: DateTime<FixedOffset>, end_date: DateTime<FixedOffset>) -> Self {
+        Self {
+            start_date,
+            end_date,
+            excluded_days: vec![],
         }
-        current_date = current_date.checked_add_days(Days::new(1)).unwrap();
     }
 
-    business_days
+    pub fn exclude_day(&mut self, day: DateTime<FixedOffset>) {
+        self.excluded_days.push(day);
+    }
+
+    pub fn calculate(&self) -> i32 {
+        let mut business_days = 0;
+        let mut current_date = self.start_date.clone();
+        while current_date <= self.end_date {
+            business_days += match current_date.weekday() {
+                Weekday::Sat | Weekday::Sun => 0,
+                _ if self.excluded_days.contains(&current_date) => 0,
+                _ => 1,
+            };
+
+            current_date = current_date.checked_add_days(Days::new(1)).unwrap();
+        }
+
+        business_days
+    }
 }
 
 #[cfg(test)]
@@ -27,10 +46,13 @@ mod tests {
         #[test]
         fn $name() {
             let (lhs, rhs, expected) = $value;
-            assert_eq!(expected, calculate_business_days_between(
+
+            let business_days = BusinessDays::new(
                 DateTime::parse_from_rfc3339(lhs).unwrap(),
-                DateTime::parse_from_rfc3339(rhs).unwrap())
+                DateTime::parse_from_rfc3339(rhs).unwrap(),
             );
+
+            assert_eq!(expected, business_days.calculate());
         }
     )*
     }
@@ -46,5 +68,22 @@ mod tests {
         gets_5_business_days_between_2021_01_01_and_2021_01_07: ("2021-01-01T00:00:00+00:00", "2021-01-07T00:00:00+00:00", 5),
         gets_6_business_days_between_2021_01_01_and_2021_01_08: ("2021-01-01T00:00:00+00:00", "2021-01-08T00:00:00+00:00", 6),
         gets_23_business_days_between_2023_03_01_and_2023_03_31: ("2023-03-01T00:00:00+00:00", "2023-03-31T00:00:00+00:00", 23),
+    }
+
+    #[test]
+    fn gets_17_business_days_between_2023_04_01_and_2023_04_30_when_excluding_easter() {
+        let mut business_days = BusinessDays::new(
+            DateTime::parse_from_rfc3339("2023-04-01T00:00:00+00:00").unwrap(),
+            DateTime::parse_from_rfc3339("2023-04-30T00:00:00+00:00").unwrap(),
+        );
+
+        business_days
+            .exclude_day(DateTime::parse_from_rfc3339("2023-04-06T00:00:00+00:00").unwrap());
+        business_days
+            .exclude_day(DateTime::parse_from_rfc3339("2023-04-07T00:00:00+00:00").unwrap());
+        business_days
+            .exclude_day(DateTime::parse_from_rfc3339("2023-04-10T00:00:00+00:00").unwrap());
+
+        assert_eq!(17, business_days.calculate());
     }
 }
